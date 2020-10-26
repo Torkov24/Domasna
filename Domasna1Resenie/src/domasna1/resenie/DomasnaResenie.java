7f80580815b7b6404ea618087753898e820b9879
User <User@DESKTOP-ISEL1H4>
2020-10-26 20:23:26 +0100

    update

--- Domasna1Resenie/src/domasna1/resenie/DomasnaResenie.java
+++ Domasna1Resenie/src/domasna1/resenie/DomasnaResenie.java
@@ -4,6 +4,7 @@ public class DomasnaResenie {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World OOP FIKT");
+		System.out.println("https://github.com/Torkov24/Domasna");
 
 	}
 
